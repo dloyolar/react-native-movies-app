8c7e0520c86913c341e9bec287aa26cdbe357705
dloyolar <diego.loyola@continuumhq.com>
2022-07-27 19:26:17 -0400

    refactor: use useMovies hook in Home

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, ActivityIndicator} from 'react-native';
+import {useMovies} from '../hooks/useMovies';
 
 export const HomeScreen = () => {
+  const {moviesInCinema, isLoading} = useMovies();
+
+  if (isLoading) {
+    return (
+      // eslint-disable-next-line react-native/no-inline-styles
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <ActivityIndicator color="red" size={100} />
+      </View>
+    );
+  }
+
   return (
     <View>
       <Text>HomeScreen</Text>
