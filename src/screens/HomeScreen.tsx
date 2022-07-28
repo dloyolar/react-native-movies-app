d2579b51e22c36138440d2360eb9a908201ddfbe
dloyolar <diego.loyola@continuumhq.com>
2022-07-27 22:38:22 -0400

    refactor: add caroursel and slider in home

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
-import {View, ActivityIndicator} from 'react-native';
+import {View, ActivityIndicator, Dimensions, ScrollView} from 'react-native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
+
+import Carousel from 'react-native-reanimated-carousel';
+
 import {MoviePoster} from '../components/MoviePoster';
 import {useMovies} from '../hooks/useMovies';
+import {HorizontalSlider} from '../components/HorizontalSlider';
+
+const {width: windowWidth} = Dimensions.get('window');
+const HALF_PAGE = windowWidth / 2;
 
 export const HomeScreen = () => {
   const {moviesInCinema, isLoading} = useMovies();
@@ -18,8 +25,35 @@ export const HomeScreen = () => {
   }
 
   return (
-    <View style={{marginTop: top}}>
-      <MoviePoster movie={moviesInCinema[0]} />
-    </View>
+    <ScrollView>
+      <View style={{marginTop: top}}>
+        <View>
+          <Carousel
+            mode="parallax"
+            // eslint-disable-next-line react-native/no-inline-styles
+            style={{
+              width: windowWidth,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+            pagingEnabled={false}
+            windowSize={2}
+            snapEnabled
+            height={420}
+            width={HALF_PAGE}
+            modeConfig={{
+              parallaxScrollingScale: 0.9,
+              parallaxScrollingOffset: 0,
+              parallaxAdjacentItemScale: 0.75,
+            }}
+            data={moviesInCinema}
+            renderItem={({item}) => <MoviePoster movie={item} />}
+          />
+        </View>
+
+        {/* POPULAR MOVIES */}
+        <HorizontalSlider movies={moviesInCinema} title="Populares" />
+      </View>
+    </ScrollView>
   );
 };
