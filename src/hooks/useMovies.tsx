227820c7445a743560349d2987f552eb56d83628
dloyolar <diego.loyola@continuumhq.com>
2022-07-28 09:39:24 -0400

    refactor: improve hook useMovies

--- src/hooks/useMovies.tsx
+++ src/hooks/useMovies.tsx
@@ -1,14 +1,43 @@
 import {useEffect, useState} from 'react';
 import {movieDB} from '../api/movieDB';
-import {Movie, MovieDBNowPlaying} from '../interfaces/movieInterface';
+import {Movie, MovieDBResponse} from '../interfaces/movieInterface';
+
+interface MoviesState {
+  nowPlaying: Movie[];
+  popular: Movie[];
+  topRated: Movie[];
+  upComing: Movie[];
+}
 
 export const useMovies = () => {
   const [isLoading, setIsLoading] = useState(true);
-  const [moviesInCinema, setMoviesInCinema] = useState<Movie[]>([]);
+  const [moviesState, setMoviesState] = useState<MoviesState>({
+    nowPlaying: [],
+    popular: [],
+    topRated: [],
+    upComing: [],
+  });
 
   const getMovies = async () => {
-    const {data} = await movieDB.get<MovieDBNowPlaying>('/now_playing');
-    setMoviesInCinema(data.results);
+    const nowPlayingPromise = movieDB.get<MovieDBResponse>('/now_playing');
+    const popularPromise = movieDB.get<MovieDBResponse>('/popular');
+    const topRatedPromise = movieDB.get<MovieDBResponse>('/top_rated');
+    const upComingPromise = movieDB.get<MovieDBResponse>('/upcoming');
+
+    const res = await Promise.all([
+      nowPlayingPromise,
+      popularPromise,
+      topRatedPromise,
+      upComingPromise,
+    ]);
+
+    setMoviesState({
+      nowPlaying: res[0].data.results,
+      popular: res[1].data.results,
+      topRated: res[2].data.results,
+      upComing: res[3].data.results,
+    });
+
     setIsLoading(false);
   };
 
@@ -16,5 +45,5 @@ export const useMovies = () => {
     getMovies();
   }, []);
 
-  return {moviesInCinema, isLoading};
+  return {...moviesState, isLoading};
 };
