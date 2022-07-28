39a01a563a78629998385ae2eafb79aaee50c434
dloyolar <diego.loyola@continuumhq.com>
2022-07-27 22:37:26 -0400

    feat: add HorizontalSlider component

---
+++ src/components/HorizontalSlider.tsx
@@ -0,0 +1,31 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import {FlatList, Text, View} from 'react-native';
+import {Movie} from '../interfaces/movieInterface';
+import {MoviePoster} from './MoviePoster';
+
+interface Props {
+  title?: string;
+  movies: Movie[];
+}
+
+export const HorizontalSlider = ({title, movies}: Props) => {
+  return (
+    <View style={{height: title ? 250 : 230}}>
+      {title && (
+        <Text style={{fontSize: 30, fontWeight: 'bold', marginLeft: 10}}>
+          {title}
+        </Text>
+      )}
+      <FlatList
+        data={movies}
+        renderItem={({item}) => (
+          <MoviePoster movie={item} width={140} height={200} />
+        )}
+        keyExtractor={item => item.id.toString()}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+      />
+    </View>
+  );
+};
